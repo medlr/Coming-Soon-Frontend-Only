477e09f35aeab1398420c9c0d22bda1a3c75bd33
SkaiLord <skailord007@gmail.com>
2023-06-07 10:20:15 +0530

    Countdown Updated

--- js/countdown.js
+++ js/countdown.js
@@ -2,43 +2,61 @@
 // COUNTDOWN
 // -----------------------------------------------------------------------------------------------------
 
-var ctd = document.getElementById('countdown');
+var ctd = document.getElementById("countdown");
 
 countdown();
 
 function countdown() {
-    // ATTENTION - Ianuary is 0, February is 1 ......
-    var launch_date = new Date(Date.UTC(2023, 5, 4, 0, 0));
-    // 11 dec
-    var days;
-    var hours;
-    var minutes;
-    var seconds;
-    var rest;
-    var now = new Date();
+  // ATTENTION - Ianuary is 0, February is 1 ......
+  var launch_date = new Date(Date.UTC(2023, 6, 18, 0, 0));
+  // 11 dec
+  var days;
+  var hours;
+  var minutes;
+  var seconds;
+  var rest;
+  var now = new Date();
 
-    seconds = rest = Math.floor(((launch_date.getTime() - now.getTime()) / 1000));
+  seconds = rest = Math.floor((launch_date.getTime() - now.getTime()) / 1000);
 
-    days = zero(Math.floor(seconds / 86400));
-    seconds -= days * 86400;
+  days = zero(Math.floor(seconds / 86400));
+  seconds -= days * 86400;
 
-    hours = zero(Math.floor(seconds / 3600));
-    seconds -= hours * 3600;
+  hours = zero(Math.floor(seconds / 3600));
+  seconds -= hours * 3600;
 
-    minutes = zero(Math.floor(seconds / 60));
-    seconds -= minutes * 60;
+  minutes = zero(Math.floor(seconds / 60));
+  seconds -= minutes * 60;
 
-    seconds = zero(Math.floor(seconds));
+  seconds = zero(Math.floor(seconds));
 
-    function zero(n) {
-        return (n < 10 ? '0' : false) + n;
-    }
+  function zero(n) {
+    return (n < 10 ? "0" : false) + n;
+  }
 
-    rest <= 0 ? days = hours = minutes = seconds = '00' : setTimeout(countdown, 1000);
+  rest <= 0
+    ? (days = hours = minutes = seconds = "00")
+    : setTimeout(countdown, 1000);
 
-    ctd.innerHTML =
-        '<li><div><span class="heading_24">' + days + '</span><br> day' + (days > 1 ? 's' : '') + '</div></li>' +
-        '<li><div><span class="heading_24 font_bold">' + hours + '</span><div class="pull-right"> hour' + (hours > 1 ? 's' : '') + '</div></div></li>' +
-        '<li><div><span class="heading_24 font_bold">' + minutes + '</span><div class="pull-right"> minute' + (minutes > 1 ? 's' : '') + '</div></div></li>' +
-        '<li><div><span class="heading_24 font_bold">' + seconds + '</span><div class="pull-right"> second' + (seconds > 1 ? 's' : '') + '</div></div></li>';
+  ctd.innerHTML =
+    '<li><div><span class="heading_24">' +
+    days +
+    "</span><br> day" +
+    (days > 1 ? "s" : "") +
+    "</div></li>" +
+    '<li><div><span class="heading_24 font_bold">' +
+    hours +
+    '</span><div class="pull-right"> hour' +
+    (hours > 1 ? "s" : "") +
+    "</div></div></li>" +
+    '<li><div><span class="heading_24 font_bold">' +
+    minutes +
+    '</span><div class="pull-right"> minute' +
+    (minutes > 1 ? "s" : "") +
+    "</div></div></li>" +
+    '<li><div><span class="heading_24 font_bold">' +
+    seconds +
+    '</span><div class="pull-right"> second' +
+    (seconds > 1 ? "s" : "") +
+    "</div></div></li>";
 }
